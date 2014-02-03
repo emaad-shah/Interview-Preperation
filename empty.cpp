315c1e599e9b0d87e01b383bd3b7fc20228c1dfa
Syed Shah <emaadshah@gmail.com>
2014-02-02 23:15:44 -0800

    Create empty.cpp

---
+++ empty.cpp
@@ -0,0 +1,18 @@
+//------------------------------------------------------------
+// Copyright: Syed Shah 2013
+// Empty File (To be used later)
+//------------------------------------------------------------
+
+
+#include <iostream>
+#include <string.h>
+#include <fstream>
+
+
+using namespace std;
+
+int main(void)
+{
+  // Add code
+  return 0;
+}
