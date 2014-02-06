136be8d3ebe4a0c930bc3516d758df5ed96ba814
Syed Shah <emaadshah@gmail.com>
2014-02-05 19:16:34 -0800

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,15 +1,18 @@
 #include "list.h"
 
+
 List::List()
 {
 	head = NULL;
 }
+
 
 // Deconstructor
 List::~List()
 {
 	free();
 }
+
 
 // Insert a node in the list
 bool List::insert(int data)
@@ -20,6 +23,7 @@ bool List::insert(int data)
 	if (newNode == NULL)
 		return false;
 	newNode->data = data;
+
 
 	// Check if newNode needs to be inserted before the head
 	if (head == NULL || newNode->data < head->data)
@@ -32,6 +36,7 @@ bool List::insert(int data)
 		Node* prev = head;
 		Node* cur = head->next;
 
+
 		// Travserse the list to find the correct location
 		while (cur != NULL && cur->data < data)
 		{
@@ -41,11 +46,32 @@ bool List::insert(int data)
 		// Add the node
 		newNode->next = cur;
 		prev->next = newNode;
-		
+
+
 	}
 	return true;
 }
 
+// Recursive insert
+void List::rInsert(int data)
+{
+	rInsertHelper(head,data);
+}
+
+// Recursive insert helper
+void List::rInsertHelper(Node *& head, int data)
+{
+	if (head == NULL || data < head->data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = head;
+		head = newNode;
+	}
+	else
+		rInsertHelper(head->next,data);
+
+}
 // Print the list
 void List::print()
 {
@@ -57,7 +83,9 @@ void List::print()
 	}
 	cout << "NULL" << endl;
 
+
 }
+
 
 // Delete a node from the list
 bool List::deleteNode(int data)
@@ -91,6 +119,39 @@ bool List::deleteNode(int data)
 	return true;
 }
 
+// Remove all node with a particular number
+void List::removeAll(int data)
+{
+	if (head == NULL)
+		return;
+	Node* tmp = head;
+	// Head of the list contains the number to be removed
+	while (tmp != NULL && tmp->data == data)
+	{
+		tmp = head;
+		head = tmp->next;
+		delete tmp;
+		tmp = NULL;
+	}
+	Node* prev = head;
+	Node* cur = head->next;
+	// Now travserse and remove all nodes
+	while (cur->next != NULL)
+	{
+		if (cur->data == data)
+		{
+			prev->next = cur->next;
+			delete cur;
+			cur = prev->next;
+		}
+		else
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+	}
+
+}
 // Delete the list / Deallocate memory
 void List::free()
 {
@@ -107,17 +168,30 @@ void List::free()
 	}
 }
 
+
 int main(void)
 {
 	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	//_CrtSetBreakAlloc(141);
 
+
 	List l1;
-	// Test Insert
-	for (int i = 5; i > -5; i--)
+	// Test recursive insert
+	for (int i = 5; i > 0; i--)
+	{
+		l1.rInsert(i);
+	}
+
+	// Testing iterative insert
+	for (int i = 0; i >= -5; i--)
 	{
 		l1.insert(i);
 	}
+	l1.insert(4);
+	l1.insert(4);
+	l1.insert(4);
+	// Test removeAll
+	l1.removeAll(4);
 	// Test Delete
 	l1.deleteNode(1);
 	l1.deleteNode(3);
@@ -128,8 +202,10 @@ int main(void)
 	l1.free();
 	l1.print();
 
+
 	// Detect memory  leak
 	_CrtDumpMemoryLeaks();
 	system("pause");
 	return 0;
 }
+
