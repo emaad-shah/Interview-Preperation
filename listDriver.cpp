161bca4f17cd186931a048049a0e9b4a0b2cc9e6
Syed Shah <emaadshah@gmail.com>
2014-02-18 01:04:04 -0800

    Update listDriver.cpp

--- listDriver.cpp
+++ listDriver.cpp
@@ -29,7 +29,15 @@ int main(void)
 	l1.free();
 	l1.print();
 
-
+	// Test List Reverse
+	l1.insert(5);
+	l1.insert(15);
+	l1.insert(1);
+	l1.insert(7);
+	l1.insert(50);
+	l1.reverse();
+	l1.print();
+	l1.free();
 	// Detect memory  leak
 	//_CrtDumpMemoryLeaks();
 	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
