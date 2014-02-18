bf1a522cab23a50b01171d6d496134b41569526b
Syed Shah <emaadshah@gmail.com>
2014-02-18 00:28:46 -0800

    Create listDriver.cpp

---
+++ listDriver.cpp
@@ -0,0 +1,39 @@
+#include "list.h"
+
+int main(void)
+{
+	List l1;
+	// Test recursive insert
+	for (int i = 5; i > 0; i--)
+	{
+		l1.rInsert(i);
+	}
+
+	// Testing iterative insert
+	for (int i = 0; i >= -5; i--)
+	{
+		l1.insert(i);
+	}
+	l1.insert(4);
+	l1.insert(4);
+	l1.insert(4);
+	// Test removeAll
+	l1.removeAll(4);
+	// Test Delete
+	l1.deleteNode(1);
+	l1.deleteNode(3);
+	l1.deleteNode(3);
+	l1.deleteNode(-3);
+	l1.deleteNode('e');
+	l1.print();
+	l1.free();
+	l1.print();
+
+
+	// Detect memory  leak
+	//_CrtDumpMemoryLeaks();
+	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetBreakAlloc(141);
+	system("pause");
+	return 0;
+}
