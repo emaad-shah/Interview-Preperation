3e701459485783812362934e4e5a97fa5a9b3971
Syed Shah <emaadshah@gmail.com>
2014-03-10 20:09:09 -0700

    Update stringToLong.cpp

--- stringToLong.cpp
+++ stringToLong.cpp
@@ -2,32 +2,39 @@
 // Copyright: Syed Shah 2014
 // Given a string, write a routine that converts the string to 
 // a long, without using the built in functions that would do 
-// this. Describe what(if any) limitations the code has.
+// this. Describe what(if any) limitations the code has
 //
 // Limitations: Compiler may throw a warning/error for values
-//				outside the range of -2147483647 to 2147483647
+//							outside the range of -2147483647 to 2147483647
 //
-//				If the first index of the string contains a 
-//				"-" symbol, the program will assume that a 
-//				string contains a negative number
+//
+// Assumptions	If the first index of the string contains a 
+//							'-' symbol, the program will assume that a 
+//							string contains a negative number
+//
+//							All non-integers except a possible negative 
+//							sign at the first index of the string will be
+//							ignored
 //------------------------------------------------------------
 
 #include <iostream>
 #include <string>
 using namespace std;
 
+// Converts a string to long and 
+// returns it to the user
 long stringToLong(const string & s)
 {
 	// Length of the string
 	int len = s.length();
 	long output = 0;
 	// Ascii code
-	int ascii_int_begin = 48;
-	int ascii_int_end = 57;
+	int ascii_begin = 48;
+	int ascii_end = 57;
 	for (long i = 0; i < len; i++)
 	{  
 		// Ignore all non-integers
-		if (s[i] >= ascii_int_begin && s[i] <= ascii_int_end)
+		if (s[i] >= ascii_begin && s[i] <= ascii_end)
 		{
 			output = output * 10 + (s[i] - '0');
 		}
@@ -66,6 +73,7 @@ int main(void)
 	test("-2147483647", -2147483647);
 	// Other Test Cases
 	test("Hello World", 1234);
+	test("+123", 123);
 
 	system("pause");
 	return 0;
